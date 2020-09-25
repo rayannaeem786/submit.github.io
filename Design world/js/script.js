f4d9349417b82df4d872708ddcde2863b56ad104
rayannaeem786 <chaudhryrayyan68@gmail.com>
2020-09-25 14:10:52 +0100

    submit

---
+++ Design world/js/script.js
@@ -0,0 +1,18 @@
+/* $(document).ready(function () {
+     $("#exampleModal").modal('show');
+ });
+
+ var games = document.getElementById("myId");
+ var texting = document.getElementById("gameText");
+ function mainFuny() {
+     var apple = document.getElementById("main-id").value;
+     if (apple !== "") {
+     texting.innerHTML = "Hi" + "&nbsp;" + apple + "!" + "&nbsp;" + "Thanks for visiting our website";
+
+     }
+      else{
+         games.style.display = "none";
+         texting.innerHTML = "OOPS you cannot visit this website Right Now";
+     };
+ };
+ 
---
+++ js/script.js
@@ -0,0 +1,16 @@
+function myfun(){
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    if (fname=="rayan" , lname=="naeem" , email=="ch@123" , password=="hello") {
+       alert("Yes You are Right")
+    } else {
+        alert("you are wrong")
+    }
+    
+   
+}
+
+
